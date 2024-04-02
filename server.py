4f90656f94f947d0e911e2f8b32bbcc351694659
Harshit M <harshitmeh111@gmail.com>
2024-04-02 15:13:24 +0530

    8

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from os import environ
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    var = environ.get("TEMP_VAR", "default")
+    return "Hello, World! " + var
 
 
 if __name__ == "__main__":
