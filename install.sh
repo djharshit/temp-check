5ac2e71a5dd0f63150bec8fe406b42c22b40084f
Harshit M <harshitmeh111@gmail.com>
2024-08-24 17:41:56 +0530

    8

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Prevent command with Service Token being recorded in bash history
+export HISTIGNORE='export DOPPLER_TOKEN*'
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo gpg --dearmor -o /usr/share/keyrings/doppler-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/doppler-archive-keyring.gpg] https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
+sudo apt-get update && sudo apt-get install doppler
+
+pip3 install -r requirements.txt
