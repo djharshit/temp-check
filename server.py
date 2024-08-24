df5a98f0e6b75b9815492338497c444e30094211
Harshit M <harshitmeh111@gmail.com>
2024-08-24 17:29:46 +0530

    chore: Remove unused environment variables and update server.py

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ from os import environ
 
 app = Flask(__name__)
 
+CLIENT_ID = environ.get("CLIENT_ID", "")
+CLIENT_HASG = environ.get("CLIENT_HASG", "")
+
 
 @app.route("/")
 def hello_world():
-    var = environ.get("TEMP_VAR", "default")
-    return "Hello, World! " + var
+    return f"Hello, World! {CLIENT_ID} {CLIENT_HASG}"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", debug=True)
